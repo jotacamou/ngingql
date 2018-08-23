01924a15f53fad4c521ab8747628d2ec0510991a
jcamou <jcamou@term0.local>
2018-08-23 16:59:53 -0700

    Remove user bits.

--- db/migrations/2_create_user.up.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE IF NOT EXISTS users (
-  id SERIAL PRIMARY KEY,
-  name character varying(255) NOT NULL,
-  email character varying(255) NOT NULL,
-  password character varying(128) NOT NULL
-);
