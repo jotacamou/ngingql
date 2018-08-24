96570d2de5f75259583f17bbdc3117d035647435
orivera <omarfranciscor@gmail.com>
2018-08-24 00:38:49 -0700

    Adding activities

---
+++ resolver/activities.go
@@ -0,0 +1,71 @@
+package resolver
+
+import (
+	"context"
+
+	"github.com/jotacamou/ngingql/db"
+)
+
+type activityResolver struct {
+	p *db.Activity
+}
+
+func (r *Resolver) AllPlaces(ctx context.Context) *[]*activityResolver {
+	var p []*activityResolver
+
+	wrapper := func(activity *db.Activity) {
+		p = append(p, &activityResolver{activity})
+	}
+
+	db.AllActivities(wrapper)
+
+	return &p
+}
+
+func (r *activityResolver) ID() int32 {
+	return r.p.ID
+}
+
+func (r *activityResolver) Title() string {
+	return r.p.Title
+}
+
+func (r *activityResolver) Description() string {
+	return r.p.Description
+}
+
+func (r *activityResolver) Owner() string {
+	return r.p.Owner
+}
+
+func (r *activityResolver) Price() float32 {
+	return r.p.Price
+}
+
+func (r *activityResolver) Fee() float32 {
+	return r.p.Feee
+}
+
+func (r *activityResolver) MainPicture() []string {
+	return r.p.MainPicture
+}
+
+func (r *activityResolver) SlideshowPicture() []string {
+	return r.p.SlideshowPicture
+}
+
+func (r *activityResolver) Duration() string {
+	return r.p.Duration
+}
+
+func (r *activityResolver) PlaceID() int32 {
+	return r.p.PlaceID
+}
+
+func (r *activityResolver) Hour() []string {
+	return r.p.Hour
+}
+
+func (r *activityResolver) Tag() []string {
+	return r.p.Tag
+}
