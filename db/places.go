02c728e55b1f280da5461f213d1324d022ba2d83
jcamou <jcamou@term0.local>
2018-08-23 17:02:59 -0700

    Break up by type/field.

---
+++ db/places.go
@@ -0,0 +1,32 @@
+package db
+
+import (
+	"log"
+
+	_ "github.com/lib/pq"
+	_ "github.com/mattes/migrate/source/file"
+)
+
+type Place struct {
+	ID      string
+	Name    string
+	Zipcode string
+	Country string
+}
+
+func AllPlaces(wrapper func(place *Place)) {
+	res, err := db.Query("SELECT id, name, zipcode, country FROM places")
+	if err != nil {
+		log.Println("error fetching all places:", err)
+	}
+	//defer db.Close()
+
+	for res.Next() {
+		place := Place{}
+		err := res.Scan(&place.ID, &place.Name, &place.Zipcode, &place.Country)
+		if err != nil {
+			log.Fatal(err)
+		}
+		wrapper(&place)
+	}
+}
---
+++ resolver/places.go
@@ -0,0 +1,39 @@
+package resolver
+
+import (
+	"context"
+
+	"github.com/jotacamou/ngingql/db"
+)
+
+type placeResolver struct {
+	p *db.Place
+}
+
+func (r *Resolver) AllPlaces(ctx context.Context) *[]*placeResolver {
+	var p []*placeResolver
+
+	wrapper := func(place *db.Place) {
+		p = append(p, &placeResolver{place})
+	}
+
+	db.AllPlaces(wrapper)
+
+	return &p
+}
+
+func (r *placeResolver) ID() string {
+	return r.p.ID
+}
+
+func (r *placeResolver) Name() string {
+	return r.p.Name
+}
+
+func (r *placeResolver) Zipcode() string {
+	return r.p.Zipcode
+}
+
+func (r *placeResolver) Country() string {
+	return r.p.Country
+}
